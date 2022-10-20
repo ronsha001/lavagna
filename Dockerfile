b43f66f7475545406bc72976daad257584a57c6f
Ron Sharabi <ronsh0111@gmail.com>
2022-10-20 13:30:09 +0300

    start

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,7 @@ FROM maven:3.6.3-jdk-8
 
 WORKDIR /lav
 
-# COPY ./src ./src
-# COPY ./pom.xml .
 COPY . .
-
-RUN mvn verify
-
-RUN apt-get update
-RUN apt-get install coreutils
 
 ENTRYPOINT [ "sh", "entrypoint.sh" ]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,11 @@ DB_USER="sa"
 DB_PASS=""
 SPRING_PROFILE="dev"
 
-while true
-do
-  continue
-done
 
 # Execute the web archive
-# java "-Xms64m -Xmx128m" -Ddatasource.dialect="$DB_DIALECT" \
-# -Ddatasource.url="$DB_URL" \
-# -Ddatasource.username="$DB_USER" \
-# -Ddatasource.password="$DB_PASS" \
-# -Dspring.profiles.active="$SPRING_PROFILE" \
-# -jar ./target/lavagna-jetty-console.war --headless
+java -Xms64m -Xmx128m -Ddatasource.dialect="$DB_DIALECT" \
+-Ddatasource.url="$DB_URL" \
+-Ddatasource.username="$DB_USER" \
+-Ddatasource.password="$DB_PASS" \
+-Dspring.profiles.active="$SPRING_PROFILE" \
+-jar ./target/lavagna-jetty-console.war --headless
